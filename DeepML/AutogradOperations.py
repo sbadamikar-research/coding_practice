dca32aa543f027c9879940f12492e900f9dccdd9
Sanchit <researchbadamikar@gmail.com>
2025-09-18 04:38:09 -0400

    Problem 26

--- DeepML/AutogradOperations.py
+++ DeepML/AutogradOperations.py
@@ -10,19 +10,54 @@ class Value:
         return f"Value(data={self.data}, grad={self.grad})"
 
     def __add__(self, other):
-        return Value((self.data + other.data), (self, other))
+        result = Value((self.data + other.data), [self, other], '+')
+        
+        def backward_fn():
+            self.grad += result.grad
+            other.grad += result.grad 
+            
+        result._backward = backward_fn
+        return result
 
     def __mul__(self, other):
-        return Value((self.data * other.data), (self, other))
+        result = Value((self.data * other.data), [self, other], '*')
+        
+        def backward_fn():            
+            self.grad += other.data * result.grad
+            other.grad += self.data * result.grad
+        
+        result._backward = backward_fn
+        return result
     
     def relu(self):
-        # Implement ReLU here
-        pass
+        result = Value(max(0, self.data), [self,], 'ReLU')
+        
+        def backward_fn():
+            self.grad += (result.data > 0) * result.grad
+            
+        result._backward = backward_fn
+        return result
     
     def backward(self):
-        # Implement backward pass here
-        pass
-
+        self.grad = 1
+        
+        backtrace = [self]
+        visited = []
+        for val in backtrace:
+            # print("Initial: ", val)
+            if (val in visited):
+                continue
+            
+            for child in val._prev:
+                # print("appending ", child.data)
+                backtrace.append(child)
+                
+            # print("Calling ", val.data)
+            val._backward()
+            visited.append(val)
+            # print("Updated: ", val)
+        
+        
 a = Value(2)
 b = Value(-3)
 c = Value(10)
